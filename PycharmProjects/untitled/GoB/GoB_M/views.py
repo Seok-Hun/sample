9ac5925e96e53a5132c27c97fba7d8777c774e28
Seok-Hun <43300837+Seok-Hun@users.noreply.github.com>
2018-11-17 00:27:17 +0900

    Update views.py

--- PycharmProjects/untitled/GoB/GoB_M/views.py
+++ PycharmProjects/untitled/GoB/GoB_M/views.py
@@ -36,7 +36,7 @@ def answer(request):
             day = int(list[1])
             date = datetime.now()
             for cho2 in cho.select('td'):
-                if day == date.day;
+                if day == date.day:
                     menu = cho2
         return JsonResponse({
             'message': {
