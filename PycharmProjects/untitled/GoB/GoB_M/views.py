5341cf94ea6563b23cd2da8a5458eac641d47568
Seok-Hun <43300837+Seok-Hun@users.noreply.github.com>
2018-11-16 22:58:12 +0900

    Update views.py

--- PycharmProjects/untitled/GoB/GoB_M/views.py
+++ PycharmProjects/untitled/GoB/GoB_M/views.py
@@ -2,13 +2,21 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import json
-import urllib.request
+import requests
+from bs4 import BeautifulSoup
+import re
+
+Tag = '#container > div.substance > div.sub_contents > table > tbody > tr '
+URL = 'http://ace.gachon.ac.kr/dormitory/reference/menu'
+req = requests.get(URL)
+html = req.text
+
+soup = BeautifulSoup(html, 'html.parser')
 
 def keyboard(request):
  
     return JsonResponse({
-        'type':'buttons',
-        'buttons':['오늘','내일']
+        'type':'text'
     })
  
 @csrf_exempt
@@ -17,30 +25,35 @@ def answer(request):
     json_str = ((request.body).decode('utf-8'))
     received_json_data = json.loads(json_str)
     datacontent = received_json_data['content']
- 
-    if datacontent == '오늘':
-        today = "오늘 급식"
- 
+    
+    if datacontent.find('오늘') != -1:
+        for cho in soup.select(Tag):
+            for cho2 in cho.select('td'):
+                a = re.sub('월|일', ' ', cho.find('th').text)
+                a[-3:]
+                list = a.split()
+                month = int(list[0])
+                day = int(list[1])
+                date = datetime.now()
+                if month == date.month and day == date.day:
+                    menu=cho2
         return JsonResponse({
                 'message': {
-                    'text': today
-                },
-                'keyboard': {
-                    'type':'buttons',
-                    'buttons':['오늘','내일']
+                    'text': '오늘 메뉴' + menu
                 }
- 
-            })
- 
-    elif datacontent == '내일':
-        tomorrow = "내일 급식"
- 
+
+    if datacontent.find('내일') != -1:
+        for cho in soup.select(Tag):
+            for cho2 in cho.select('td'):
+                a = re.sub('월|일', ' ', cho.find('th').text)
+                a[-3:]
+                list = a.split()
+                month = int(list[0])
+                day = int(list[1])
+                date = datetime.now()
+                if month == date.month and day == date.day+1:
+                    menu=cho2
         return JsonResponse({
                 'message': {
-                    'text': tomorrow
-                },
-                'keyboard': {
-                    'type':'buttons',
-                    'buttons':['오늘','내일']
-                }
-        })
+                    'text': '내일 메뉴' + menu
+                } 
