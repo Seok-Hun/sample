995e8a11fc79448c6ea7c9751992fbe81e47440b
Seok-Hun <43300837+Seok-Hun@users.noreply.github.com>
2018-11-17 02:15:53 +0900

    Update views.py

--- PycharmProjects/untitled/GoB/GoB_M/views.py
+++ PycharmProjects/untitled/GoB/GoB_M/views.py
@@ -18,7 +18,7 @@ soup = BeautifulSoup(html, 'html.parser')
 def keyboard(request):
     return JsonResponse({
         "type": "buttons",
-        "buttons":["오늘","내일"]
+        "buttons": ["오늘",'내일']
     })
 
 
@@ -27,43 +27,88 @@ def answer(request):
     json_str = ((request.body).decode('utf-8'))
     received_json_data = json.loads(json_str)
     datacontent = received_json_data['content']
+    time = datetime.now()
+    date = time.day
 
-    if datacontent=='오늘':
-        for cho in soup.select(Tag):
-            a = re.sub('월|일', ' ', cho.find('th').text)
-            a[-3:]
-            list = a.split()
-            day = int(list[1])
-            date = datetime.now()
-            for cho2 in cho.select('td'):
-                if day == date.day:
-                    menu = cho2.text
+    if datacontent == '오늘':
         return JsonResponse({
-            'message': {
-                'text': menu
-            },
             'keyboard': {
                 'type': 'buttons',
-                'buttons':['오늘','내일']
+                'buttons': ['아침','점심','저녁']
             }
         })
 
-    elif datacontent=='내일':
+    elif datacontent == '내일':
+        date=date+1
+        return JsonResponse({
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['아침','점심','저녁']
+            }
+        })
+
+    if datacontent == '아침':
+        menu = [0,0,0]
+        i = 0
         for cho in soup.select(Tag):
             a = re.sub('월|일', ' ', cho.find('th').text)
             a[-3:]
             list = a.split()
             day = int(list[1])
-            date = datetime.now()
             for cho2 in cho.select('td'):
-                if day == date.day + 1:
-                    menu = cho2.text
+                if day == date.day:
+                    menu[i] = cho2.text
+                    i+=1
         return JsonResponse({
             'message': {
-                'text': menu
+                menu[0]
             },
             'keyboard': {
                 'type': 'buttons',
-                'buttons':['오늘','내일']
+                'buttons': ['오늘','내일']
+            }
+        })
+
+    if datacontent == '점심':
+        menu = [0,0,0]
+        i = 0
+        for cho in soup.select(Tag):
+            a = re.sub('월|일', ' ', cho.find('th').text)
+            a[-3:]
+            list = a.split()
+            day = int(list[1])
+            for cho2 in cho.select('td'):
+                if day == date.day:
+                    menu[i] = cho2.text
+                    i+=1
+        return JsonResponse({
+            'message': {
+                menu[1]
+            },
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['오늘','내일']
+            }
+        })
+
+    if datacontent == '저녁'
+        menu = [0,0,0]
+        i = 0
+        for cho in soup.select(Tag):
+            a = re.sub('월|일', ' ', cho.find('th').text)
+            a[-3:]
+            list = a.split()
+            day = int(list[1])
+            for cho2 in cho.select('td'):
+                if day == date.day:
+                    menu[i] = cho2.text
+                    i+=1
+        return JsonResponse({
+            'message': {
+                menu[2]
+            },
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['오늘','내일']
             }
         })
